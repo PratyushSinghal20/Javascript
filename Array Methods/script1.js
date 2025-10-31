3e0bae87956df1a18f7a0151fb669f487c37fd6d
PratyushSinghal20 <pratyush1820@gmail.com>
2025-10-31 17:49:42 +0530

    Initial Commit

---
+++ Array Methods/script1.js
@@ -0,0 +1,22 @@
+// const fruits = ['Mango' , 'Apple' , 'Orange']
+
+// const myfruits = fruits
+
+// myfruits.push('Dates')
+// myfruits.push('grapes')
+
+// const user1 = {
+
+//     firstName : 'Anurag',
+//     lastName : 'Singh',
+// }
+
+// const user2 = user1 
+
+// user2.lastName = 'Pandey' 
+
+
+
+const username1 = 'Pratyush'
+
+let username2 = username1
