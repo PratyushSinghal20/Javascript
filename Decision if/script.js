dd9c268a89da851b192acb4324273622961f22a8
PratyushSinghal20 <pratyush1820@gmail.com>
2025-11-10 15:06:46 +0530

    Initial Commit

---
+++ Decision if/script.js
@@ -0,0 +1,21 @@
+const username = "Akash"
+const userAge = 28
+
+
+console.log(`Name : ${username}`);
+console.log(`Age : ${userAge}` );
+
+
+if(userAge >= 18 && userAge <= 24) {
+     console.log("user is a working professional");
+}
+
+
+
+else{
+     console.log("user is not a working professional ")
+}
+
+debugger
+
+console.log("Program Ended!")
