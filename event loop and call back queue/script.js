589b1a3494e87072e9927e706f247015cb6c6b87
PratyushSinghal20 <pratyush1820@gmail.com>
2025-11-15 12:42:52 +0530

    Initial commit

---
+++ event loop and call back queue/script.js
@@ -0,0 +1,15 @@
+console.log('Hi - 1')
+
+function hello() {
+    console.log('Hello World');
+}
+
+debugger
+setTimeout(function() {
+    console.log('Hi-3')
+}, 0)
+
+for (let i = 0; i<=4 ; i++) {
+    console.log(i);
+}
+console.log('Hi - 2');
