d6c7262abd3d76efa14cad8fbba4eec0f8f362aa
PratyushSinghal20 <pratyush1820@gmail.com>
2025-11-19 14:49:45 +0530

    Initial Commit

---
+++ for loop/script.js
@@ -0,0 +1,20 @@
+console.log("Program Started")
+
+debugger
+
+// for(let i =0 ; i < 10; i++ ) {
+//     console.log(i);
+    
+// }
+
+
+
+for(let i = 0; i <= 10; i++) {
+    if(i % 2 === 0) {
+        console.log(i); 
+    }
+}
+
+
+
+console.log("Program Ended")
