2ad0c30920c2ce601e54cb3cded8f2ecd5e09884
PratyushSinghal20 <pratyush1820@gmail.com>
2025-11-21 20:52:57 +0530

    Initial Commit

---
+++ freeze object seal object/script.js
@@ -0,0 +1,22 @@
+const user = {
+    firstName : 'Adarsh',
+    lastName : 'Singh',
+    pata : {
+        city : 'Banglore',
+        pinCode : 947472,
+        state : 'Karnataka',
+        moreDetails : {
+            population : 478349284,
+            area : '736 sq km',
+        },
+    },
+    age : 15,
+    isGraduate : false,
+}
+
+user = {}
+
+let username = 'Anurag'
+
+// Object.seal(user)
+Object.freeze(user)
