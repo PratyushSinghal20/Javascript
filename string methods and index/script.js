bb044d633ccbd4143781c627fa740596a7a40c1b
PratyushSinghal20 <pratyush1820@gmail.com>
2025-12-21 17:21:29 +0530

    Initial Commit

---
+++ string methods and index/script.js
@@ -0,0 +1,15 @@
+const message = "hello world!"
+
+const faltuMessage = "      hi , i am anurag.          "
+
+const finalMessage = faltuMessage.trim()
+
+const modified = message.toUpperCase()
+
+
+const capitalMessage = message.toUpperCase()
+
+const lastfourdigits = '7856'
+
+const maskedAccountNumber = lastfourdigits.padStart(16 , '*')
+
