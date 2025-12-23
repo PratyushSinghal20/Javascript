cdb50fb804dd00ffa9c2e899538383c980029c86
PratyushSinghal20 <pratyush1820@gmail.com>
2025-12-23 08:55:52 +0530

    Initial Commit

---
+++ switch case/script.js
@@ -0,0 +1,74 @@
+
+// if(dayNumber === 0) {
+    //     console.log('It is sunday today')
+    // }
+    // else if (dayNumber ===1) {
+        //     console.log('It is monday today')
+        // }
+// else if(dayNumber === 2) {
+    //     console.log('it is tuesday today')
+    // }
+    // else if(dayNumber === 3) {
+        //     console.log('it is wed today')
+// }else if (dayNumber === 4) {
+//    console.log('It is Thursday Today')
+//  } else if (dayNumber === 5) {
+//    console.log('It is Friday Today')
+//  } else if (dayNumber === 6) {
+    //    console.log('It is Saturday Today')
+//  } else {
+//    console.log('Please Enter a Valid Day Number')
+//  }
+
+// const dayNumber = 0
+
+// debugger
+
+// switch(dayNumber) {
+//     case 0:
+//         console.log('It is sunday today')
+//         console.log("hello")
+//         break
+
+//     case 1:
+//         console.log('It is monday today')  
+//         break  
+
+//     case 2:
+//         console.log('It is tuesday today')
+//         break
+        
+//     default :
+//     console.log('Enter a valid number')    
+// } 
+
+// console.log("program ended")
+
+const grade = 'c'
+
+switch(grade) {
+    case 'a':
+        console.log("you are excellent")
+        break
+
+    case 'b' :
+        console.log("you are good") 
+        break
+        
+    case 'c' :
+        console.log("you are average")
+        break
+        
+    case 'd' :
+        console.log(" you are below average") 
+        break
+
+    case 'e' :
+        console.log("you are bad")
+        break
+        
+    default :
+        console.log("Done")    
+}
+
+console.log("Program Ended!")
