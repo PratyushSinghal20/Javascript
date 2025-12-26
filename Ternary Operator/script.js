563673033a31c08af72ff9ff8aaf7c8bbef072d7
PratyushSinghal20 <pratyush1820@gmail.com>
2025-12-26 22:31:08 +0530

    Initial Commit

---
+++ Ternary Operator/script.js
@@ -0,0 +1,14 @@
+// const username = 5 > 2 ? 'Pratyush' : 20
+
+// console.log(username)
+
+// const gender = 'F'
+
+// const userMessage = `${gender == 'F' ? 'She' : 'He' } is a college student.`
+
+// console.log(userMessage)
+
+
+const result = 0 ? 'Anurag' : 'Singh' ? '12' : 0
+
+console.log(result)
