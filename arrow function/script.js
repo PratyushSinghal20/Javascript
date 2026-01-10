cd9b7c46b585d77296df97f46353af37c1e5a41f
PratyushSinghal20 <pratyush1820@gmail.com>
2026-01-10 11:27:48 +0530

    Initial Commit

---
+++ arrow function/script.js
@@ -0,0 +1,18 @@
+// function square(num) {
+//     return num*num
+// }
+
+// const square = function(num) {
+//     return num * num
+// }
+
+const square = (num) => {
+    return num * num
+}
+
+const add = (a,b) => a+b
+
+const random = () => Math.random()
+
+setTimeout(() => { console.log('hiii');
+} , 2000)
