06673d34a277a4a8b51737557e7b7d83ecd31ebb
PratyushSinghal20 <pratyush1820@gmail.com>
2026-07-15 12:01:17 +0530

    practice on javascript

---
+++ practice/script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector("button")
+
+
+let body = document.querySelector("body")
+
+button.addEventListener("click", function() {
+    body.style.backgroundColor = "black"
+    body.style.color = "white"
+})  
